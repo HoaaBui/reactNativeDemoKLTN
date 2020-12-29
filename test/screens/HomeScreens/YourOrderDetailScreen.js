9626d8538df565a1524635726663caf2ec943a0b
HoaBui <hoa652109@gmail.com>
2020-12-29 12:02:24 +0700

    Update orderDetailScreen

---
+++ test/screens/HomeScreens/YourOrderDetailScreen.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Timeline from 'react-native-timeline-flatlist';
+
+const YourOrderDetailScreen = () => {
+  const [dataa, setData] = useState([ 
+    {time: '09:00', title: 'Event 1', description: 'Event 1 Description'},
+    {time: '10:45', title: 'Event 2', description: 'Event 2 Description'},
+    {time: '12:00', title: 'Event 3', description: 'Event 3 Description'},
+    {time: '14:00', title: 'Event 4', description: 'Event 4 Description'},
+    {time: '16:30', title: 'Event 5', description: 'Event 5 Description'},
+  ]);
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          {/* <Text>Your order detail screen.</Text> */}
+          <Timeline
+            data={dataa}
+          />
+        </View>
+      );
+}
+
+export default YourOrderDetailScreen;
